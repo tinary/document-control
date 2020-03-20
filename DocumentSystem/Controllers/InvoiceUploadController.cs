838b7dd52e6d6e72ea6cd5b868f5110ab07c88ed
Tina Wang <tinary@users.noreply.github.com>
2020-03-20 14:56:22 -0400

    update files

--- DocumentSystem/Controllers/InvoiceUploadController.cs
+++ DocumentSystem/Controllers/InvoiceUploadController.cs
@@ -8,128 +8,128 @@ using DocumentSystem.Models;
 
 namespace DocumentSystem.Controllers
 {
-    public class InvoiceUploadController : BaseController
+	public class InvoiceUploadController : BaseController
 	{
-				/// <summary>
-        /// GET: Index
-        /// </summary>        
-        /// <returns>Return index view</returns>
-				public ActionResult Index()
+		/// <summary>
+		/// GET: Index
+		/// </summary>        
+		/// <returns>Return index view</returns>
+		public ActionResult Index()
+		{
+			UploadDocModel model = new UploadDocModel { File = null, FileList = new List<InvoiceDetailsModel>() };
+
+			try
+			{
+				model.FileList = _db.spGetetAllFiles().Select(p => new InvoiceDetailsModel
 				{
-					UploadDocModel model = new UploadDocModel { File = null, FileList = new List<InvoiceDetailsModel>() };
+					InvoiceID = p.invoice_id,
+					InvoicePO = p.invoice_PO,
+					InvoiceMIME = p.invoice_MIME
+				}).ToList();
 
-					try
-					{
-						model.FileList = _db.spGetetAllFiles().Select(p => new InvoiceDetailsModel
-						{
-							InvoiceID = p.invoice_id,
-							InvoicePO = p.invoice_PO,
-							InvoiceMIME = p.invoice_MIME
-						}).ToList();
-
-					}
-					catch (Exception ex)
-					{
-						Console.Write(ex);
-					}
-					return View(model);
-				}
+			}
+			catch (Exception ex)
+			{
+				Console.Write(ex);
+			}
+			return View(model);
+		}
 
 
-				/// <summary>
-        /// POST: Index
-        /// </summary>
-        /// <param name="model">Model parameter</param>
-        /// <returns>Return response information</returns>
-				[HttpPost]
-        [ValidateAntiForgeryToken]
-				public ActionResult Index(UploadDocModel model)
+		/// <summary>
+		/// POST: Index
+		/// </summary>
+		/// <param name="model">Model parameter</param>
+		/// <returns>Return response information</returns>
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Index(UploadDocModel model)
+		{
+			string fileName = string.Empty;
+			byte[] fileContent;
+			string fileExt = string.Empty;
+
+			try
+			{
+				if (ModelState.IsValid)
 				{
-					string fileName = string.Empty;
-					byte[] fileContent;
-					string fileExt = string.Empty;
+					byte[] uploadedFile = new byte[model.File.InputStream.Length];
+					model.File.InputStream.Read(uploadedFile, 0, uploadedFile.Length);
 
-				try
-					{
-						if (ModelState.IsValid)
-						{
-							byte[] uploadedFile = new byte[model.File.InputStream.Length];
-							model.File.InputStream.Read(uploadedFile, 0, uploadedFile.Length);
+					fileName = Path.GetFileNameWithoutExtension(model.File.FileName);
+					fileExt = model.File.ContentType;
+					fileContent = uploadedFile;
 
-							fileName = Path.GetFileNameWithoutExtension(model.File.FileName);
-							fileExt = model.File.ContentType;
-							fileContent = uploadedFile;
-
-							//save file to database
-							_db.spAddFile(fileName, fileExt, fileContent);
-
-						}
-
-						model.FileList = _db.spGetetAllFiles().Select(p => new InvoiceDetailsModel
-						{
-							InvoiceID = p.invoice_id,
-							InvoicePO = p.invoice_PO,
-							InvoiceMIME = p.invoice_MIME
-						}).ToList();
-
-
-					}
-					catch (Exception ex)
-					{
-						Console.Write(ex);
-					}
-
-					return View(model);
+					//save file to database
+					_db.spAddFile(fileName, fileExt, fileContent);
 
 				}
 
-				/// <summary>
-				/// Download files to view or download physical copy if broswer doesn't support the file type
-				/// </summary>
-				/// <param name="fileId">File ID parameter</param>
-				/// <returns>Return download file</returns>
-				public ActionResult DownloadFile(int fileId)
+				model.FileList = _db.spGetetAllFiles().Select(p => new InvoiceDetailsModel
 				{
-					UploadDocModel model = new UploadDocModel { File = null, FileList = new List<InvoiceDetailsModel>() };
-
-					try
-					{
-						var fileInfo = _db.spGetFileDetails(fileId).First();
-
-						return this.GetFile(fileInfo.invoice_document, fileInfo.invoice_MIME);
-					}
-					catch (Exception ex)
-					{
-						Console.Write(ex);
-					}
-
-					return View(model);
-
-				}
+					InvoiceID = p.invoice_id,
+					InvoicePO = p.invoice_PO,
+					InvoiceMIME = p.invoice_MIME
+				}).ToList();
 
 
+			}
+			catch (Exception ex)
+			{
+				Console.Write(ex);
+			}
 
-				/// <summary>
-        /// Get file method
-        /// </summary>
-        /// <param name="fileContent">File content parameter</param>
-        /// <param name="fileType">File content type parameter</param>
-        /// <returns>Returns file</returns>
-				private FileResult GetFile(byte[] fileContent, string fileType)
-				{
-					FileResult file = null;
+			return View(model);
 
-					try
-					{
-						byte[] byteContent = fileContent;
-						file = this.File(byteContent, fileType);
-					}
-					catch (Exception ex)
-					{
-						throw ex;
-					}
+		}
 
-					return file; 
-				}
-    }
+		/// <summary>
+		/// Download files to view or download physical copy if broswer doesn't support the file type
+		/// </summary>
+		/// <param name="fileId">File ID parameter</param>
+		/// <returns>Return download file</returns>
+		public ActionResult DownloadFile(int fileId)
+		{
+			UploadDocModel model = new UploadDocModel { File = null, FileList = new List<InvoiceDetailsModel>() };
+
+			try
+			{
+				var fileInfo = _db.spGetFileDetails(fileId).First();
+
+				return this.GetFile(fileInfo.invoice_document, fileInfo.invoice_MIME);
+			}
+			catch (Exception ex)
+			{
+				Console.Write(ex);
+			}
+
+			return View(model);
+
+		}
+
+
+
+		/// <summary>
+		/// Get file method
+		/// </summary>
+		/// <param name="fileContent">File content parameter</param>
+		/// <param name="fileType">File content type parameter</param>
+		/// <returns>Returns file</returns>
+		private FileResult GetFile(byte[] fileContent, string fileType)
+		{
+			FileResult file = null;
+
+			try
+			{
+				byte[] byteContent = fileContent;
+				file = this.File(byteContent, fileType);
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+
+			return file;
+		}
+	}
 }
--- DocumentSystem/Controllers/POViewerController.cs
+++ DocumentSystem/Controllers/POViewerController.cs
@@ -9,64 +9,64 @@ using DocumentSystem.BAL;
 
 namespace DocumentSystem.Controllers
 {
-    public class POViewerController : BaseController
-    {
+	public class POViewerController : BaseController
+	{
 
-				public ActionResult Index()
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+
+		/// <summary>
+		/// Find purchase order, if not found return error message
+		/// </summary>
+		/// <param name="model">a model</param>
+		/// <returns>return response information</returns>
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Index(POViwerModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				POViewerServices PSvc = new POViewerServices();
+				model = PSvc.GetPODocument(model.PONumber);
+
+				if (model == null)
 				{
-					 return View();
+					ModelState.AddModelError("PONumber", "Purchase order not found or matched");
+					return View("Index", model);
 				}
+			}
+			return View("Index", model);
+		}
 
 
-				/// <summary>
-				/// Find purchase order, if not found return error message
-				/// </summary>
-				/// <param name="model">a model</param>
-				/// <returns>return response information</returns>
-				[HttpPost]
-				[ValidateAntiForgeryToken]
-				public ActionResult Index(POViwerModel model)
+		/// <summary>
+		/// Get purchase order
+		/// </summary>
+		/// <param name="PONumber">a string parameter</param>
+		/// <returns>return purchase order file</returns>
+		public ActionResult GetPOFile(string PONumber)
+		{
+			POViwerModel POViewModel = new POViwerModel();
+
+			try
+			{
+				POViewerServices PSvc = new POViewerServices();
+				POViewModel = PSvc.GetPODocument(PONumber);
+
+				if (POViewModel.InvoiceDocument.Length > 0)
 				{
-					if (ModelState.IsValid)
-					{
-						POViewerServices PSvc = new POViewerServices();
-						model = PSvc.GetPODocument(model.PONumber);
-
-						if (model == null)
-						{
-							ModelState.AddModelError("PONumber", "Purchase order not found or matched");
-							return View("Index", model);
-						}
-					}
-					 return View("Index",model);
+					return File(POViewModel.InvoiceDocument, POViewModel.InvoiceMIME);
 				}
+			}
+			catch (Exception ex)
+			{
+				Console.Write(ex);
+			}
+			return View("Index", POViewModel);
+		}
 
-
-				/// <summary>
-				/// Get purchase order
-				/// </summary>
-				/// <param name="PONumber">a string parameter</param>
-				/// <returns>return purchase order file</returns>
-				public ActionResult GetPOFile(string PONumber)
-				{
-					POViwerModel POViewModel = new POViwerModel();
-
-					try
-					{
-						POViewerServices PSvc = new POViewerServices();
-						POViewModel = PSvc.GetPODocument(PONumber);
-
-						if (POViewModel.InvoiceDocument.Length > 0)
-						{
-							return File(POViewModel.InvoiceDocument, POViewModel.InvoiceMIME);
-						}
-					}
-					catch (Exception ex)
-					{
-						Console.Write(ex);
-					}
-					return View("Index", POViewModel);
-				}
-		
 	}
 }
